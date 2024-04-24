7a6be9c1f1c227820b4a3f1806313db257e72044
Joe Pattenden <151946049+JojojoMohojo@users.noreply.github.com>
2024-04-24 20:11:03 +0100

    add mouseover listender to turn squares black

--- javascript.js
+++ javascript.js
@@ -3,9 +3,8 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 256; i++) {
     const square =  document.createElement("div");
     square.id = "square" + i;
+    square.addEventListener("mouseover", () => {
+        square.classList.add("black");
+    })
     container.appendChild(square);
-    console.log(`Appended square${i}`)
 }
-
-const pageHeight = document.body.scrollHeight;
-console.log("Page height:", pageHeight, "pixels");
